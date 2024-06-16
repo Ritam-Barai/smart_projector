f08d8874815a1437229f8c9cc1a7218a046e4bf7
Ritam-Barai <20d070064@iitb.ac.in>
2024-06-17 01:44:49 +0530

    Minor change

--- slideshow_script.sh
+++ slideshow_script.sh
@@ -7,6 +7,16 @@ FILE_PATH=$HOME/smart_projector
 
 cd "$FILE_PATH" || exit
 
+VENV_PATH="venv/bin/activate"
+
+# Check if the virtual environment exists
+if [ -f "$VENV_PATH" ]; then
+    echo "Activating the virtual environment..."
+    source "$VENV_PATH"
+else
+    echo "Virtual environment not found. Skipping activation."
+fi
+
 python3 main.py -l $DISPLAY
 echo "Exiting current session..."
 exit 0
